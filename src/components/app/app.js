a16ee99e39d06a75b9ffe5fc8ea2536644d0fe2e
Vladislav Polyukhov <vlad.polyukhov@gmail.com>
2020-05-19 16:16:49 +0300

    Update app.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,7 @@ export default class App extends Component {
     items: [
       { id: 1, label: 'Drink Coffee', important: false, done: false },
       { id: 2, label: 'Learn React', important: true, done: false },
-      { id: 3, label: 'Make Awesome App', important: false, done: false }
+      { id: 3, label: 'Make An Awesome App', important: false, done: false }
     ],
     filter: 'all',
     search: ''
